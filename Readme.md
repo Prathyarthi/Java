95b1f1dab249204ffbceca200ea7ee2288e19b04
Prathyarthi <prathyarti@gmail.com>
2023-11-09 20:16:57 +0530

    Added sum program, inital files

---
+++ Readme.md
@@ -0,0 +1,1 @@
+These are the programs in JAVA for different concepts
--- variables_datatypes/Sum.java
+++ variables_datatypes/Sum.java
@@ -1,4 +1,4 @@
-public class Sum_pgm{
+public class Sum{
     public static void main(String args[]){
         int a = 2;
         int b = 3;
