58134ba02bcbb06c78dbe1c0735b899f38ce606d
Prathyarthi <prathyarti@gmail.com>
2023-11-16 22:34:27 +0530

    Added Pass or Fail Program

---
+++ conditional_statements/Pass_Fail.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Pass_Fail{
+    public static void main(String args[]){
+        int marks = 35;
+
+        // Using ternary operator : 
+        String result = marks>=33? "Pass" : "Fail";
+        System.out.println(result);
+    }
+}
