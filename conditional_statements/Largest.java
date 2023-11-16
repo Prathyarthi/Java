4c9e3c8b0759a4c13ca27b77674b7dfbfb7a6fcc
Prathyarthi <prathyarti@gmail.com>
2023-11-16 22:09:43 +0530

    Added largest of 2 numbers

---
+++ conditional_statements/Largest.java
@@ -0,0 +1,13 @@
+public class Largest{
+    public static void main(String args[]){
+        int a = 5;
+        int b = 2;
+        if(a>=b){
+            System.out.println("A is larger");
+        }
+        else{
+            System.out.println("B is larger");
+        }
+    }
+
+}
